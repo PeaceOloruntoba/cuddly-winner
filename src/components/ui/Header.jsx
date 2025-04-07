03f48b64e298e588aaf3fa23330dab7f11009be0
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 20:42:04 +0100

    Update Header.jsx

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -6,8 +6,8 @@ export default function Header() {
     <div className="py-3 px-6 w-full flex text-xl font-semibold shadow-md shadow-black items-center justify-between">
       <img src="https://aul.edu.ng/students/storage/photo/logo.png" alt="" className='h-12' />
       <div className="flex gap-8">
-        <NavLink>Testimonies</NavLink>
-        <NavLink>Share your testimony</NavLink>
+        <NavLink to={"/"}>Testimonies</NavLink>
+        <NavLink to={"/"}>Share your testimony</NavLink>
       </div>
     </div>
   );
