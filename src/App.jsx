2a917f3ef44b5bffa777572fcfbb54ccd60944dc
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 18:29:05 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { Toaster } from 'sonner';
+import React, { Suspense } from "react";
+import { Toaster } from "sonner";
+import Spinner from "./components/shared/Spinner";
 
 export default function App() {
   return (
     <>
       <Toaster position="top-right" richColors />
+      <Suspense
+        fallback={
+          <div className="w-screen h-screen flex items-center justify-center text-center">
+            <Spinner />
+          </div>
+        }
+      ></Suspense>
     </>
   );
 }
