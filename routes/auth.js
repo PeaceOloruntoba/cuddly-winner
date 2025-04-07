91512896e5b1ca87cf9b44e5f6cbb837bb82755e
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 18:49:56 +0100

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+import express from "express";
+const router = express.Router();
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import Admin from "../models/Admin.js";
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const admin = await Admin.findOne({ email });
+    if (!admin) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = await admin.comparePassword(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ adminId: admin._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({ token });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+export default router;
