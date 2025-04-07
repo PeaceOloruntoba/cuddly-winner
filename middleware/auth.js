334792d1003e26cd8305e9f2f1a210126aa68b3c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 19:49:25 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,13 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
-  const token = req.header("Authorization");
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+
+  const token = authHeader.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "No token, authorization denied" });
