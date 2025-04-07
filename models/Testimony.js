c088810c4ffb2fd41043b5b478f1db573aa03de4
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 18:52:34 +0100

    Update Testimony.js

--- models/Testimony.js
+++ models/Testimony.js
@@ -1,13 +1,15 @@
 // backend/models/Testimony.js
 import mongoose from "mongoose";
 
-const testimonySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  testimony: { type: String, required: true },
-},
-{
-   timestamps
-});
+const testimonySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: false },
+    testimony: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Testimony = mongoose.model("Testimony", testimonySchema);
 export default Testimony;
