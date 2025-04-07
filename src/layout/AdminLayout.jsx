396a723d920902ab4c4a3f4b99234fd410ab3f7c
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 20:23:43 +0100

    sss

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Header from "../components/ui/Header";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Foorter from "../components/ui/Foorter";
+import useAdminStore from "../store/useAdminStore";
+import "./Watermark.css";
+import Spinner from "../components/shared/Spinner";
 
 export default function AdminLayout() {
+  const token = useAdminStore((state) => state.token);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+  }, [token, navigate]);
+
+  if (!token) {
+    return <Spinner />;
+  }
+
   return (
-    <div className="w-screen h-screen flex flex-col">
+    <div className="w-screen h-screen flex flex-col admin-layout">
       <Header />
       <Outlet />
       <Foorter />
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Header from "../components/ui/Header";
 import { Outlet } from "react-router";
 import Foorter from "../components/ui/Foorter";
+// import "./Watermark.css";
 
 export default function RootLayout() {
   return (
-    <div className="w-screen h-screen flex flex-col">
+    <div className="w-screen h-screen flex flex-col root-layout">
       <Header />
       <Outlet />
       <Foorter />
