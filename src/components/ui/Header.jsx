02cfaba6bb20353b2dcc71d1bc86fa48fd818ba8
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-04-07 20:41:08 +0100

    Update Header.jsx

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router'
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="py-3 px-6 w-full flex text-xl font-semibold shadow-md shadow-black items-center justify-between">
+      <img src="https://aul.edu.ng/students/storage/photo/logo.png" alt="" className='h-12' />
+      <div className="flex gap-8">
+        <NavLink>Testimonies</NavLink>
+        <NavLink>Share your testimony</NavLink>
+      </div>
+    </div>
+  );
 }
